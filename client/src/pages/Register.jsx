516ea6613e180b6e85da89cf0da254ab22d172a1
darosaleh <darosaleh@gmail.com>
2024-03-25 13:34:57 +0000

    Auth System Complete

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,13 @@
+import { useContext } from "react";
 import {Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
+import { AuthContext } from "../context/AuthContext";
+
 const Register = () => {
-    return  <>
-    <Form>
+
+   const {registerInfo, updateRegisterInfo, registerUser, registerError, isRegisterLoading} = useContext(AuthContext)
+    return  (
+    <>
+    <Form onSubmit={registerUser}>
         <Row style={{
             height: "100vh",
             justifyContent: "center",
@@ -10,21 +16,39 @@ const Register = () => {
             <Col xs={6}>
             <Stack gap={3}>
                 <h2>Register</h2>
-                <Form.Control type="text" placeholder="Name"/>
-                <Form.Control type="email" placeholder="Email"/>
-                <Form.Control type="password" placeholder="Password"/>
+                <Form.Control
+                 type="text" 
+                 placeholder="Name"
+                  onChange={(e) => 
+                  updateRegisterInfo({...registerInfo,name:e.target.value})
+                  }
+                  />
+                <Form.Control type="email" placeholder="Email"
+                onChange={(e) => 
+                  updateRegisterInfo({...registerInfo,email:e.target.value})
+                  }
+                  />
+                <Form.Control type="password" placeholder="Password" 
+                onChange={(e) => 
+                  updateRegisterInfo({...registerInfo,password:e.target.value})
+                  }
+                  />
                 <Button variant="primary" type="submit">
-                    Register
+                    {isRegisterLoading ? "Creating your account" : "Register"}
                 </Button>
+                {
+                    registerError?.error && (<Alert variant="danger"><p>{registerError?.message}</p></Alert>)
+                }
 
-                <Alert variant="danger"><p>An error occurred </p></Alert>
+                
 
 
             </Stack>
             </Col>
         </Row>
     </Form>
-    </>   ;
+    </>   
+    );
 }
  
 export default Register;
