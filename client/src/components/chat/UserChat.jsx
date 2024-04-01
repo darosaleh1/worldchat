4bb4e890b1219d712642c53368d4fedfb6119b57
darosaleh <darosaleh@gmail.com>
2024-04-01 13:51:58 +0100

    Real time chat (socket.io)

--- client/src/components/chat/UserChat.jsx
+++ client/src/components/chat/UserChat.jsx
@@ -1,9 +1,15 @@
 import { useFetchRecipientUser } from "../../hooks/useFetchRecipient";
 import avatar from "../../assets/avatar.svg"
 import { Stack } from "react-bootstrap";
+import { useContext } from "react";
+import { ChatContext } from "../../context/ChatContext";
 
 const UserChat  = ({chat,user}) => {
     const {recipientUser} = useFetchRecipientUser(chat, user)
+    const {onlineUsers} = useContext(ChatContext)
+
+    const isOnline = onlineUsers?.some((user) => user?.userId === recipientUser?._id)
+
 
     return (<Stack 
         direction="horizontal" 
@@ -23,7 +29,7 @@ const UserChat  = ({chat,user}) => {
         <div className="d-flex flex-column align-items-end">
         <div className="date">12/-2/2022</div>
             <div className="this-user-notifications">2</div>
-            <span className="user-online"></span>
+            <span className={isOnline ?"user-online" : ""}></span>
         </div>
         </Stack>
         );
